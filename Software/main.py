d2ed68415a4f87472b5cdd3a55c875d69f79dfd9
DeniskaRediska21 <lenis@lenis>
2023-11-25 18:32:16 +0300

    changed keys

--- Software/main.py
+++ Software/main.py
@@ -114,9 +114,9 @@ keyboard.keymap = [
 
         KC.N1,          KC.N2,           KC.N3,           KC.N4,           KC.N5,                           KC.N6,               KC.N7,           KC.N8,     KC.N9,    KC.N0,\
 
-        KC.LCTL(KC.A),  KC.LCTL(KC.S),   KC.NO,           SEL_LINE,        KC.HOME,                         KC.AUDIO_VOL_UP,     KC.LEFT,         KC.UP,     KC.RIGHT, KC.DEL,\
+        KC.LCTL(KC.A),  KC.LCTL(KC.S),   KC.NO,           SEL_LINE,        KC.HOME,                         KC.MUTE,     KC.LEFT,         KC.UP,     KC.RIGHT, KC.DEL,\
 
-        KC.LCTL(KC.Z),  KC.LCTL(KC.X),   KC.LCTL(KC.C),   KC.LCTL(KC.V),   KC.END,                          KC.AUDIO_VOL_DOWN,   KC.QUOT,   KC.DOWN,   KC.NO,    KC.SLSH,\
+        KC.LCTL(KC.Z),  KC.LCTL(KC.X),   KC.LCTL(KC.C),   KC.LCTL(KC.V),   KC.END,                          KC.GRAVE,   KC.QUOT,   KC.DOWN,   KC.NO,    KC.SLSH,\
  
                                                           KC.RSFT,           KC.RALT,                        KC.TAB,              KC.LCTL
 
